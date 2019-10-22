0cc4cb6574f3e97fcd461957b14bd461745ebdb8
Oblivion <kiaraedwards@pursuit.org>
2019-10-22 02:29:14 -0400

    almost done

--- classes_lab.js
+++ classes_lab.js
@@ -185,13 +185,13 @@ class Movie{
         this.description = description;
     };
     blurb(){
-        return this.name+' came out in '+this.year+'. \nIt\'s a '+this.genre+' movie starring '+this.cast[0]+ '  '+this.description
+        return this.name+" came out in "+this.year+", It's in "+this.genre+". Cast: "+this.cast[0]+ " "+this.description
     };
 };
 
 let myMovie = new Movie("Brideshead Revisited", 2008, "Britsh Drama", 
  ["Matthew Goode as Charles Ryder,Ben Whishaw as Lord Sebastian Flyte, Hayley Atwell as Lady Julia Flyte, ect." ],
-"Befriended by aristocrat Sebastian Flyte (Ben Whishaw), Oxford student Charles Ryder (Matthew Goode) finds that the power and privilege experienced by the family is seductive. On a visit to Brideshead, the ancestral home, he falls in love with his friend's sister, Julia (Hayley Atwell). However, as Charles' ties to Sebastian and family deepen, he finds himself at odds with their strong Roman Catholicism.",);
+"Befriended by aristocrat Sebastian Flyte (Ben Whishaw), Oxford student Charles Ryder (Matthew Goode) finds that the power and privilege  experienced by the family is seductive. On a visit to Brideshead, the ancestral home, he falls in love with his friend's sister, Julia (Hayley Atwell). Howe-ver, as Charles' ties to Sebastian and family deepen, he finds himself at odds with their strong Roman Catholicism.",);
 
 console.log(myMovie.blurb())
 
@@ -240,10 +240,68 @@ console.log("The sum of ", vec1, " and ", vec2, " is ", vec1.plus(vec2));
 console.log("The difference of ", vec1, " and ", vec2, " is ", vec1.minus(vec2));
 
 let v3 = new Vector(3, 4)
-console.log('The length of ',v3, ' is ', v3.getLength());
+console.log("The length of ",v3, " is ", v3.getLength());
+
+
+
+
+// ## Question 7
+
+// a. Write a class called `Cylinder` that has properties `radius` and `height`.  Create an instance of a Cylinder.
+
+// b. Add an instance method `getVolume` that returns the [volume](https://www.mathopenref.com/cylindervolume.html)
+
+// c. Add an instance method `getSurfaceArea` that returns the [surface area](https://www.mathopenref.com/cylinderareamain.html)
+
+
+class Cylinder{
+    constructor(radius, height){
+        this.radius = radius;
+        this.height = height;
+    }
+    getVolume(){
+
+    }
+}
+
+
+
+// ## Question 8
+
+// [Dates in JavaScript](https://www.digitalocean.com/community/tutorials/understanding-date-and-time-in-javascript#targetText=The%20Date%20object%20is%20a,the%20current%20date%20and%20time.)
+
+// a. Write a class called `Post` that has properties `datePosted`, `user`, and `text`.  Create an array of `Post` objects.
+
+// b. Create an instance method that returns whether or not the post was made today.
+
+// c. Filter your array of `Post` objects to only include posts made today.
 
 
 
 
 
 
+// ## Question 9
+
+// a. Make a class called `Car` with properties `make` and `model`.  Create an instance of a `Car`
+
+// b. Make a class called `Bike` with properties `gears` and `hasBell`.  Create an instance of `Bike`
+
+// c. Give each class a static method called `numberOfWheels` that returns the number of wheels (2 for bikes, 4 for cars). 
+//  Why does it make sense for this to be a static method instead of an instance method?
+
+
+
+
+
+
+// ## Question 10
+
+// a. Make a class called `Vehicle` with properties `color` and `name`.  Give it a method called `makeSound` which logs "WHHOOSSSH" 
+// to the console
+
+// b. Modify your `Car` and `Bike` classes from Question 7 to extend the `Vehicle` class.
+
+// c. Create a new `Bike` instance that has a `color` of "green" and `name` "Bikey McBikeface"
+
+// d. Create a new `Car` instance that has a `color` of "red" and `name` "Carry McCarface"
